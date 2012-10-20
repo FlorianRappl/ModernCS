579ce028fe7a967029edef8e4e96a7273254c9d6
Florian Rappl <rappl.florian@gmail.com>
2012-10-20 15:30:07 +0200

    Mehr LINQ Demos
    Demos über Mengentheorie mit LINQ - Intersection, Union, Except und
    SymmetricExcept.

--- 06-LINQ/Program.cs
+++ 06-LINQ/Program.cs
@@ -32,6 +32,9 @@ namespace ModernCS
 
             //Suchen wir mal Duplikate
             LinqFindDuplicate(xmlDoc);
+
+            //Verwenden wir mal Mengentheorie
+            LinqSetTheory();
         }
 
         static void LinqSqlSyntax(XDocument doc)
@@ -70,7 +73,7 @@ namespace ModernCS
             numbers[6] = 9; //7 -> 9
 
             //Ausgegeben: 0, 4, 9, 16, 25, 36, 81, 100
-            PrintResults("Defered Execution", result);
+            PrintResults("Verzögerte Ausführung", result);
 
             //Queries werden erst zur Ausgabe berechnet - d.h. solange wir keine FIXE Liste erzeugen (z.B. mit ToList(), ToArray(), ...), die einen Zustand
             //fixiert, wird das Query immer bei der Ausgabe berechnet (teuer und u.u. nicht die Ausgabe die man wollte)
@@ -86,10 +89,30 @@ namespace ModernCS
                              .Select(m => m.Key); //Unseren Schlüssel wählen wir am Ende aus
 
             //Die Duplikate ausgeben (Duplikate sind in diesem Fall diese, die die selbe Textausgabe (direkte Unterknotenwerte verbunden)
-            PrintResults("Duplicates", duplicates);
+            PrintResults("Duplikate", duplicates);
         }
 
-        static void PrintResults(string name, IEnumerable<string> elements)
+        static void LinqSetTheory()
+        {
+            var half = "abcdefghijklmn".ToCharArray();
+            var odd = "bdfhjlnprtvxz".ToCharArray();
+
+            var union = half.Union(odd).ToArray();
+            var intersection = half.Intersect(odd).ToArray();
+            var except = half.Except(odd).ToArray();
+            var symmetricExcept = half.SymmetricExcept(odd).ToArray();
+
+            //Alles (aber keine (!) Duplikate), d.h. erste Hälfte des Alphabets komplett und 2. Hälfte nur jedes 2.
+            PrintResults("Vereinigungsmenge", union);
+            //Nur was in half UND odd vorkommt, d.h. nur jedes 2. Element der ersten Hälfte des Alphabets (ab dem 2.)
+            PrintResults("Schnittmenge", intersection);
+            //Nur was NUR in half vorkommt, d.h. nur jedes 2. Element der ersten Hälfte des Alphabets (ab dem 1.)
+            PrintResults("Differenzmenge", except);
+            //Alles was in half und ODD vorkommt, ohne dass was in beiden vorkommt, d.h. jedes 2. Element der ersten Hälfte (ab dem 1.) und jedes 2. Element (ab 2.) der 2. Hälfte des Alphabets
+            PrintResults("Symmetrische Differenz", symmetricExcept);
+        }
+
+        static void PrintResults<T>(string name, IEnumerable<T> elements)
         {
             Console.WriteLine("------------");
             Console.WriteLine(name);
@@ -99,6 +122,17 @@ namespace ModernCS
             {
                 Console.WriteLine(element);
             }
+        }
+    }
+
+    //Um uns Schreibarbeit zu sparen (erst nützlich bei mehrmaligen (min. 2) Verwendung) legen wir eine Erweiterungsmethode an
+    static class LinqSymmetric
+    {
+        //Verwenden Generics (im LINQ Stil)
+        public static IEnumerable<T> SymmetricExcept<T>(this IEnumerable<T> left, IEnumerable<T> right)
+        {
+            //Nutzen nur bereits vorhandene LINQ Methoden
+            return left.Union(right).Except(left.Intersect(right));
         }
     }
 }
